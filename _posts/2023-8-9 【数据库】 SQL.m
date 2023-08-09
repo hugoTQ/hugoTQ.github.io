0e9a0dc5ed2e79987394718c1fa9cde23c98a935
hugoTQ <hugo.tqhuang@gmail.com>
2023-08-09 11:49:28 +0800

    Update 2023-8-9 【数据库】 SQL.m

--- _posts/2023-8-9 【数据库】 SQL.m
+++ _posts/2023-8-9 【数据库】 SQL.m
@@ -5,5 +5,12 @@ select * from t1
 left join t2
 on t1.a=xxx
 
-与 select * from t1 条数一样。也就是说left join是on t1表t2表的关联条件已经确定基表条数，on加入t1条件不会影响基表条数 
+与 select * from t1  条数一样。也就是说left join是on t1表t2表的关联条件已经确定基表条数，on加入t1条件不会影响基表条数 
+
+
+select * from t1
+join t2
+on t1.a=xxx
+
+与 select * from t1 where  t1.a=xxx 条数差异相对较少
 
